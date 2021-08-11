0f89bdc8f26ac6c9ec43b0d0f4db5fb477f5a334
Kuss <2360314753@qq.com>
2021-08-11 20:37:30 +0800

    Revert ":bug: fix: babel.config -> preset-env not supports es5"
    This reverts commit 6998f10f0330c12eea7ccbcbaa6104921ed6c0a7.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   presets: [
     '@babel/preset-typescript',
-    '@babel/preset-env'
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '12',
+        },
+      },
+    ],
   ]
 }
