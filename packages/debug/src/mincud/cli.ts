e50aa7beb65af1bced6dd9e625cdeec7da27fe2b
Kuss <2360314753@qq.com>
2021-08-31 11:24:57 +0800

    :recycle: refactor: execa to run command and openQRCode

--- packages/debug/src/mincud/cli.ts
+++ packages/debug/src/mincud/cli.ts
@@ -1,67 +1,42 @@
-import concurrently from 'concurrently'
-import WebSocket, { Data } from 'ws';
-import chalk from 'chalk'
-import slash from 'slash'
-import { DEBUG_PORT, LogLevel } from './shared'
-import { logToConsole } from './logToConsole';
+import open from 'open'
+import internalIp from 'internal-ip'
+import execa from 'execa'
+import stripAnsi from 'strip-ansi';
+import { startServer } from './server';
+import { StringMatcher } from './StringMatcher';
+import { REGEXP_INTERNAL_IP, REGEXP_LOCALHOST } from './shared';
 
 const args = process.argv.slice(2)
 
-const SERVER_BIN = slash(__filename)
+const openQRCode = (text: string) => {
+  let origin = text
+  if (origin.match('localhost')) {
+    origin = text.replace('localhost', internalIp.v4.sync())
+  }
+  const url = new URL(origin)
+  url.searchParams.set('devSecrect', 'iNCUDeveloper++')
+  const subtitle = "请打开 「南大家园」 - 「生活板块」 - 右上角「扫一扫」，扫描以上二维码，开始调试"
 
-const CON_OPTS: concurrently.Options = {
-  raw: true
-}
-
-type RecvType = 'log'
-
-interface Received {
-  type: RecvType
-  level: LogLevel,
-  data: string[]
-}
-
-const formatMessage = (message: Data) => {
-  const str = message.toString()
-  return JSON.parse(str) as Received
-}
-
-export const startServer = () => {
-
-  const wss = new WebSocket.Server({ port: DEBUG_PORT });
-
-  wss.on('connection', (ws) => {
-    ws.on('message', message => {
-      const { type, level, data } = formatMessage(message)
-      if (type === 'log' && data.length > 0) {
-        logToConsole(level, data)
-      }
-    });
-  });
-
-  wss.on('error', (err) => {
-    console.log(err)
-  })
-
-  console.log(chalk.cyan(`
-  ****     **** ** ****     **   ******  **     **
-  /**/**   **/**/**/**/**   /**  **////**/**    /**
-  /**//** ** /**/**/**//**  /** **    // /**    /**
-  /** //***  /**/**/** //** /**/**       /**    /**
-  /**  //*   /**/**/**  //**/**/**       /**    /**
-  /**   /    /**/**/**   //****//**    **/**    /**
-  /**        /**/**/**    //*** //****** //******* 
-  //         // // //      ///   //////   ///////  
-            Welcome to MINCU Damon!
-        !Fast - !Scalable - !Integrated
-  
-        listening on ws://localhost:2333
-  `))
+  open(`https://qrcode-function.vercel.app/api?text=${encodeURIComponent(url.toString())}&title=${origin}&subtitle=${subtitle}&type=html`)
 }
 
 export const startCli = () => {
-  concurrently(
-    [`node -e "require('${SERVER_BIN}').startServer()"`, ...args],
-    CON_OPTS
-  )
+  startServer()
+  if (args.length === 0) return
+
+  const stringMatcher = new StringMatcher([REGEXP_INTERNAL_IP, REGEXP_LOCALHOST])
+  stringMatcher.onMatch(matchRes => {
+    openQRCode(matchRes[0])
+  })
+
+  const { stderr, stdout } = execa.command(args[0], { env: { FORCE_COLOR: 'true' } })
+
+  stdout?.on('data', data => {
+    const str = stripAnsi(data.toString())
+    stringMatcher.put(str)
+
+    process.stdout.write(data)
+  })
+
+  stderr?.pipe(process.stderr)
 }
--- packages/debug/src/mincud/shared.ts
+++ packages/debug/src/mincud/shared.ts
@@ -1,5 +1,8 @@
 import { _window } from 'mincu-lib'
 
+export const REGEXP_INTERNAL_IP = /(http:\/\/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+):([0-9]+))/g
+export const REGEXP_LOCALHOST = /(http:\/\/localhost:([0-9]+))/g
+
 export const DEBUG_PORT = 2333
 export const DEBUG_HOST = _window.location?.hostname || 'localhost'
 export const LEVELS: LogLevel[] =
