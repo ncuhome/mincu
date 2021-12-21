d28e0b4b3162cc997c93f0ee24bdf33319c24b5e
Kuss <2360314753@qq.com>
2021-12-21 16:25:11 +0800

    :wrench: chore: remove configurations of typedoc, tsdoc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
-  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-tsdoc'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
     ecmaVersion: 2018,
     sourceType: 'module',
-  },
-  rules: {
-    'tsdoc/syntax': 'warn',
-  },
+  }
 }
