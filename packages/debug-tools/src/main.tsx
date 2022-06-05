bf560fe7da79cd3286f985bf92999c8b55333e66
Kuss <kussssss@outlook.com>
2022-06-05 18:17:59 +0800

    fix: debug-tools and deps upgraded

--- packages/debug-tools/src/main.tsx
+++ packages/debug-tools/src/main.tsx
@@ -1,5 +1,6 @@
-import { render } from 'preact'
+import { createRoot } from 'react-dom/client'
 import { App } from './app'
 import 'virtual:windi.css'
 
-render(<App />, document.getElementById('app')!)
+const root = createRoot(document.getElementById('app')!)
+root.render(<App />)
