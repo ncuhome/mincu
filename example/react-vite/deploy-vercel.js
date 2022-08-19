b6b684fae2fdfaec21be1fca0c72ebcf11117c96
Kuss <kussssss@outlook.com>
2022-08-19 19:15:21 +0800

    feat: example/react-vite deploy-vercel

---
+++ example/react-vite/deploy-vercel.js
@@ -0,0 +1,33 @@
+// mincu-react-vite
+const { execSync } = require('child_process');
+const pkgJson = require('./package.json')
+
+const fs = require('fs');
+const path = require('path');
+
+const buildJson = Object.assign({}, pkgJson)
+
+const { dependencies, devDependencies } = buildJson
+
+const rewrite = (deps) => {
+  for (const [key, value] of Object.entries(deps)) {
+    deps[key] = value.replace('workspace:', '')
+  }
+}
+
+rewrite(dependencies)
+rewrite(devDependencies)
+
+fs.writeFileSync('./package.json', JSON.stringify(buildJson, null, 2))
+
+try {
+  execSync('vercel --prod', {
+    stdio: 'inherit'
+  })
+}
+catch (error) {
+  throw error
+}
+finally {
+  fs.writeFileSync(path.join(__dirname, './package.json'), JSON.stringify(pkgJson, null, 2))
+}
